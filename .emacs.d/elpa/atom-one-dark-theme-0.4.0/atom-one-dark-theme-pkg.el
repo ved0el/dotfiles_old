8d6fce21d9a15c67849b5718c354c2d50e3fa274
bl4cklotu5@XPS13 <lqh5318@gmail.com>
2020-02-24 02:03:40 +0900

    [delete] Remove Emacs config

--- .emacs.d/elpa/atom-one-dark-theme-0.4.0/atom-one-dark-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "atom-one-dark-theme" "0.4.0" "Atom One Dark color theme" 'nil :commit "c2ae343971f8cda7f5b5392552ce9281f52e53de" :authors '(("Jonathan Chu" . "me@jonathanchu.is")) :maintainer '("Jonathan Chu" . "me@jonathanchu.is") :url "https://github.com/jonathanchu/atom-one-dark-theme")
