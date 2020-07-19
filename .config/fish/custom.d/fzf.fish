fff6455188dc1e74661785e4e157b4f6e4180bfd
bl4ckl0tu5@XPS13 <lqh5318@gmail.com>
2020-07-20 03:57:37 +0900

    Modify 'fzf' command

--- .config/fish/custom.d/fzf.fish
+++ .config/fish/custom.d/fzf.fish
@@ -1,22 +1,22 @@
-set -Ux FZF_COMPLETION_TRIGGER '**'
-set -Ux FZF_TMUX 1
-set -Ux FZF_TMUX_OPTS "-h 80% -d 70%"
-set -Ux FZF_PREVIEW_DIR_CMD "tree -C {} 2> /dev/null"
-set -Ux FZF_PREVIEW_FILE_CMD "bat {} 2> /dev/null"
-set -Ux FZF_PREVIEW_CMD "if test -f {}; \
-                          $FZF_PREVIEW_FILE_CMD; \
+set -U FZF_TMUX 1
+set -U FZF_TMUX_HEIGHT "80%"
+set -U FZF_TMUX_OPTS "-d 70%"
+set -U FZF_PREVIEW_DIR_COMMAND "tree -C {} 2> /dev/null"
+set -U FZF_PREVIEW_FILE_COMMAND "bat {} 2> /dev/null"
+set -U FZF_PREVIEW_COMMAND "if test -f {}; \
+                          $FZF_PREVIEW_FILE_COMMAND; \
                         else if test -d {}; \
-                          $FZF_PREVIEW_DIR_CMD; \
+                          $FZF_PREVIEW_DIR_COMMAND; \
                         else; \
                           echo {}; \
-                        end; "
-set -Ux FZF_DEFAULT_CMD "fd --type file --follow --hidden --color=always --exclude .git $HOME"
-set -Ux FZF_DEFAULT_OPTS "--ansi \
-                          --preview '$FZF_PREVIEW_CMD' \
-                          --preview-window=:hidden:right:50% \
-                          --bind='?:toggle-preview' \
-                          --bind='ctrl-u:preview-page-up' \
-                          --bind='ctrl-d:preview-page-down'"
-set -Ux FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND --select-1 --exit-0"
-set -Ux FZF_ALT_C_CMD "fd --type directory --follow --hidden $HOME"
-set -Ux FZF_CTRL_R_OPTS "--sort --exact"
+                        end;"
+set -U FZF_DEFAULT_COMMAND "fd --type file --follow --hidden --color=always --exclude .git . $HOME"
+set -U FZF_DEFAULT_OPTS "--layout=reverse --inline-info
+                        --preview '$FZF_PREVIEW_COMMAND' \
+                        --preview-window=:hidden:right:60% \
+                        --bind='?:toggle-preview' \
+                        --bind='ctrl-u:preview-page-up' \
+                        --bind='ctrl-d:preview-page-down'"
+set -U FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND --select-1 --exit-0"
+set -U FZF_ALT_C_COMMAND "fd --type directory --follow --hidden --color=always --exclude .git . $HOME"
+set -U FZF_CTRL_R_OPTS "--sort --exact"
