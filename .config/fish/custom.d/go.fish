c613333247430c733ab313c598649c9134b35186
bl4ckl0tu5@XPS13 <lqh5318@gmail.com>
2020-12-31 21:29:12 +0900

    Update /Users/bl4ckl0tu5/.pyenvs/bin:/Users/bl4ckl0tu5/src/go/bin:/Users/bl4ckl0tu5/src/github.com/bl4ckl0tu5/dotfiles/bin:/Users/bl4ckl0tu5/.cargo/bin:/Users/bl4ckl0tu5/.nvm:/opt/homebrew/bin:/opt/homebrew/sbin:/opt/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/Library/Apple/usr/bin:/Users/bl4ckl0tu5/.nvm/versions/node/v15.4.0/bin for Golang

--- .config/fish/custom.d/go.fish
+++ .config/fish/custom.d/go.fish
@@ -1,3 +1,5 @@
 # Confign for Golang
 set -U GOPATH $HOME/src/go
 set -U GOBIN $GOPATH/bin
+set -U GOARCH arm64
+set -U GOOS darwin
--- .config/fish/custom.d/user_path.fish
+++ .config/fish/custom.d/user_path.fish
@@ -1,3 +1,5 @@
 # Add user paths
-set -U fish_user_paths $fish_user_paths $GOBIN $DOTFILE/bin (yarn global bin)
+set -U fish_user_paths /opt/homebrew/bin /opt/homebrew/sbin /opt/local/bin /usr/bin /bin /usr/sbin /sbin
+set -U fish_user_paths $PYENV_ROOT/bin $GOBIN $DOTFILE/bin $HOME/.cargo/bin $HOME/.nvm $fish_user_paths
 set -x TERM xterm-256color
+set -U PYENV_ROOT $HOME/.pyenvs
