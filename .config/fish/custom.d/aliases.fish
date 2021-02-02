4102dc733b619ebfab03663a6be31a45988d3be1
bl4ckl0tu5@XPS13 <lqh5318@gmail.com>
2021-02-03 06:03:31 +0900

    [update] Remove some UNIX aliases, and some new

--- .config/fish/custom.d/aliases.fish
+++ .config/fish/custom.d/aliases.fish
@@ -5,34 +5,15 @@ alias ....="cd ../../.."
 alias .....="cd ../../../.."
 
 # grep
+alias grep="rg"
 alias grep="grep --color=auto"
 
 # ls
-alias ls="lsd"
+alias ls="exa"
 alias l="ls -l"
 alias la="ls -a"
 alias lla="ls -la"
 alias lt="ls --tree"
-
-# remove
-alias rmf="sudo rm -rfi"
-
-# yay
-alias yi="yay -S"
-alias yr="yay -Rs"
-alias ys="yay -Ss"
-alias yu="yay -Syu"
-alias yar="yay -Qdtq | yay -Rs -"
-alias ysi="yay -Si"
-alias yrm="yay -Scc"
-alias yli="yay -Qqe"
-
-# apt
-alias sau="sudo apt-fast update && sudo apt-fast upgrade"
-alias sap="sudo apt-fast purge"
-alias sai="sudo apt-fast install"
-alias saa="sudo apt-fast autoremove -y"
-alias acs="apt-cache search"
 
 # git
 alias gpl="git pull"
@@ -41,10 +22,30 @@ alias gcm="git commit"
 alias gch="git checkout"
 alias gst="git status"
 
-# top
-alias top="htop"
+
+# # yay
+# alias yi="yay -S"
+# alias yr="yay -Rs"
+# alias ys="yay -Ss"
+# alias yu="yay -Syu"
+# alias yar="yay -Qdtq | yay -Rs -"
+# alias ysi="yay -Si"
+# alias yrm="yay -Scc"
+# alias yli="yay -Qqe"
+
+# # apt
+# alias sau="sudo apt-fast update && sudo apt-fast upgrade"
+# alias sap="sudo apt-fast purge"
+# alias sai="sudo apt-fast install"
+# alias saa="sudo apt-fast autoremove -y"
+# alias acs="apt-cache search"
 
 # Others
+alias top="htop"
+alias cat="bat"
+alias find="fd"
+alias wc="tokei"
+alias od="hexyl"
 alias reboot="sudo reboot now"
 alias shutdown="sudo shutdown"
 alias tmuxconf="code ~/.tmux.conf"
