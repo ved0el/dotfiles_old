d418416dacb6cecde1842bb743d62d3ea220ad7f
K@MBP <lqh5318@gmail.com>
2021-07-22 06:38:57 +0900

    Removing 'fcd' command

--- .config/fish/function.d/fcd.fish
+++ .config/fish/function.d/fcd.fish
@@ -1,9 +1,9 @@
-function fcd -d "cd to directory using fzf"
-  fd --type directory --follow --hidden . $HOME | \
-  fzf-tmux -d 80% --reverse --preview 'tree -C {}' \
-           --preview-window=:right:60% --bind='?:toggle-preview' | \
-  read select
-  [ -n "$select" ]; and cd "$select"
-  echo " $select "
-  commandline -f repaint
-end
+# function fcd -d "cd to directory using fzf"
+#   fd --type directory --follow --hidden . $HOME | \
+#   fzf-tmux -d 80% --reverse --preview 'tree -C {}' \
+#            --preview-window=:right:60% --bind='?:toggle-preview' | \
+#   read select
+#   [ -n "$select" ]; and cd "$select"
+#   echo " $select "
+#   commandline -f repaint
+# end
