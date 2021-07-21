2bc8d444bbe8cb276217e556a922c418f43f07d7
K@MBP <lqh5318@gmail.com>
2021-07-22 06:38:30 +0900

    Change fzf config

--- .config/fish/custom.d/fzf.fish
+++ .config/fish/custom.d/fzf.fish
@@ -1,23 +1,15 @@
-set -U FZF_LEGACY_KEYBINDINGS 0
-set  FZF_TMUX 1
-set  FZF_TMUX_HEIGHT "80%"
-set  FZF_TMUX_OPTS "-d 70%"
-set -U FZF_PREVIEW_DIR_COMMAND "tree -C {} 2> /dev/null"
-set -U FZF_PREVIEW_FILE_COMMAND "bat {} 2> /dev/null"
-set -U FZF_PREVIEW_COMMAND "if test -f {}; \
-                          $FZF_PREVIEW_FILE_COMMAND; \
-                        else if test -d {}; \
-                          $FZF_PREVIEW_DIR_COMMAND; \
-                        else; \
-                          echo {}; \
-                        end;"
-set -U FZF_DEFAULT_COMMAND "fd --type file --follow --hidden --color=always --exclude .git . $HOME"
-set -U FZF_DEFAULT_OPTS "--layout=reverse --inline-info \
-                        --preview '$FZF_PREVIEW_COMMAND' \
-                        --preview-window=:hidden:right:60% \
-                        --bind='?:toggle-preview' \
-                        --bind='ctrl-u:preview-page-up' \
-                        --bind='ctrl-d:preview-page-down'"
-set -U FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND --select-1 --exit-0"
-set -U FZF_ALT_C_COMMAND "fd --type directory --follow --hidden --color=always --exclude .git . $HOME"
-set -U FZF_CTRL_R_OPTS "--sort --exact"
+# Change the files searched
+set fzf_fd_opts --hidden --exclude=.git,.DS_Store
+
+# Change hotkey, see `fzf_configure_bindings --help` for futher infomations.
+fzf_configure_bindings --directory=\cf --git_log=\cl --variables
+
+
+# Force `fzf.fish` using fzf-tmux
+function fzf --wraps=fzf --description="Use fzf-tmux if in tmux session"
+  if set --query TMUX
+    fzf-tmux $argv
+  else
+    command fzf $argv
+  end
+end
