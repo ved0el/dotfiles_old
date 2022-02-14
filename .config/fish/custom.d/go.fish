32bb8e7882c53cc63e33b47fed4983bc226ad5cc
K@MBP <lqh5318@gmail.com>
2022-02-14 23:24:46 +0900

    Add GOPATH and GOBIN env variables

--- .config/fish/custom.d/go.fish
+++ .config/fish/custom.d/go.fish
@@ -1,5 +1,5 @@
 # Confign for Golang
-# set -U GOPATH $HOME/src/go
-# set -U GOBIN $GOPATH/bin
-set -U GOARCH arm64
-set -U GOOS darwin
+set -Ux GOPATH $HOME/src/go
+set -Ux GOBIN $GOPATH/bin
+set -Ux GOARCH arm64
+set -Ux GOOS darwin
